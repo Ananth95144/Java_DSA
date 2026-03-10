3902a0590ab778b48de61225633dd7052a3e49b5
Boovananthan <Boovananthan95144@gmail.com>
2026-03-10 10:18:51 +0530

    Add Basic class with main method and array initialization

---
+++ Day1/test2.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+class Basic {
+	    
+	public static void main(String[] args) {
+		Basic ob = new Basic();
+		int arr[]= {10,20,30};
+	}
+}
